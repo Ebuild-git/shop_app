ceeb61c9ed2223e575139e91d4e339394c6f2650
Ebuild-git <achref.hamouda@e-build.tn>
2024-03-04 15:10:45 +0100

    up

--- app/Http/Controllers/InformationsController.php
+++ app/Http/Controllers/InformationsController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class InformationsController extends Controller
 {
     public function index(){
-        return view("admin.informations.index");  
+        return view("Admin.informations.index");  
     }
 }
