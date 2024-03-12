d4b5736d6ed0f34c707b2fc7a7504744aba781f1
Ebuild-git <achref.hamouda@e-build.tn>
2024-03-12 16:58:22 +0000

    update

--- resources/views/components/alert-livewire.blade.php
+++ resources/views/components/alert-livewire.blade.php
@@ -7,7 +7,7 @@
     </div>
 @enderror
 
-@if (session()->has('info'))
+@if (session()->has('error'))
     <div class="alert alert-danger alert-dismissible" role="alert">
         {{ session('error') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
