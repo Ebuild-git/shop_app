335f3c4223645d3ffe0c953c2a647ca0a116bd36
kg <48618359+kevingassam@users.noreply.github.com>
2024-03-15 22:55:58 +0100

    post

--- resources/views/User/post.blade.php
+++ resources/views/User/post.blade.php
@@ -3,6 +3,20 @@
 @section('content')
 @section('body')
 
+<!-- ======================= Shop Style 1 ======================== -->
+<section class="bg-cover" style="background:url('/icons/post.jpg') no-repeat;">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                <div class="text-left py-5 mt-3 mb-3">
+                    <h1 class="ft-medium mb-3 text-white">Vendre un article</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ======================= Shop Style 1 ======================== -->
+
 <div class="container pt-5 pb-5">
     
     <div >
