609c0435e04cdf7d8f495d482f370ef130cc6b2f
Ebuild-git <achref.hamouda@e-build.tn>
2024-03-18 13:19:55 +0000

    update prix

--- app/Livewire/User/Prix.php
+++ app/Livewire/User/Prix.php
@@ -17,13 +17,12 @@ class Prix extends Component
     {
         $post = posts::join('sous_categories', 'posts.id_sous_categorie', '=', 'sous_categories.id')
         ->join('categories', 'sous_categories.id_categorie', '=', 'categories.id')
-        ->where('categories.luxury', true)
         ->select("categories.pourcentage_gain","posts.prix")
         ->first();
         $pourcentage_gain = $post->pourcentage_gain;
         //$frais_livraison = $post->frais_livraison;
 
-        $this->prix = round($post->prix + (($pourcentage_gain/$post->prix) * 100), 3);
+        $this->prix = round($post->prix + (($pourcentage_gain*$post->prix) / 100), 3);
         return view('livewire.user.prix');
     }
 }
