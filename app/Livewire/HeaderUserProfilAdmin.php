6ab4a4c080cd9050d836687dacf43c96910b4957
Ebuild-git <achref.hamouda@e-build.tn>
2024-03-26 11:18:44 +0000

    up

--- app/Livewire/HeaderUserProfilAdmin.php
+++ app/Livewire/HeaderUserProfilAdmin.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Events\UserEvent;
+use App\Models\notifications;
 use App\Models\User;
 use Livewire\Component;
 
@@ -37,7 +39,19 @@ class HeaderUserProfilAdmin extends Component
     {
         if (is_null($this->user->photo_verified_at)) {
             $this->user->photo_verified_at = now();
+           
+            //make notification
+            event(new UserEvent($this->user->id));
+            $notification = new notifications();
+            $notification->titre = "Votre photo de profil a été validé !";
+            $notification->id_user_destination  = $this->user->id;
+            $notification->type = "alerte";
+            $notification->destination = "user";
+            $notification->message = "Nous vous informons que votre photo de profil a été validé par les administrateurs";
+            $notification->save();
+
             $this->dispatch('alert', ['message' => "Photo accepté !",'type'=>'info']);
+            
         } else {
             $this->user->photo_verified_at = null;
             $this->dispatch('alert', ['message' => "La Photo a été refusée !","type"=>"info"]);
