ffe52955a006090e0f87762df665b480ef876725
kg <48618359+kevingassam@users.noreply.github.com>
2024-04-10 12:37:39 +0200

    up

--- app/Livewire/User/Shopinners.php
+++ app/Livewire/User/Shopinners.php
@@ -14,29 +14,40 @@ class Shopinners extends Component
     use WithPagination;
     public function render()
     {
-        $userId = auth()->user()->id;
+        if (Auth::check()) {
+            $userId = auth()->user()->id;
 
-        $shopiners = User::select(
-            'users.id',
-            'users.name',
-            'users.avatar',
-            'users.username',
-            'users.certifier',
-            DB::raw('AVG(ratings.etoiles) as average_rating'),
-            DB::raw('COUNT(posts.id) as total_posts')
-        )
-            ->leftJoin('ratings', 'users.id', '=', 'ratings.id_user_rated')
-            ->leftJoin('posts', 'users.id', '=', 'posts.id_user')
-            ->leftJoin('pings', function ($join) use ($userId) {
-                $join->on('users.id', '=', 'pings.pined')
-                    ->where('pings.id_user', $userId);
-            })
-            ->where('users.role', '!=', 'admin')
-            ->groupBy('users.id', 'users.name', 'users.avatar', 'users.username', 'users.certifier', 'pings.id_user')
-            ->orderByRaw('CASE WHEN pings.id_user IS NOT NULL THEN 0 ELSE 1 END') // Met les "pings" en premier
-            ->orderByDesc('average_rating') // Ensuite, trie par note moyenne
-            ->orderByDesc('total_posts')
-            ->paginate(50);
+            $shopiners = User::select(
+                'users.id',
+                'users.name',
+                'users.avatar',
+                'users.username',
+                'users.certifier',
+                DB::raw('AVG(ratings.etoiles) as average_rating'),
+                DB::raw('COUNT(posts.id) as total_posts')
+            )
+                ->leftJoin('ratings', 'users.id', '=', 'ratings.id_user_rated')
+                ->leftJoin('posts', 'users.id', '=', 'posts.id_user')
+                ->leftJoin('pings', function ($join) use ($userId) {
+                    $join->on('users.id', '=', 'pings.pined')
+                        ->where('pings.id_user', $userId);
+                })
+                ->where('users.role', '!=', 'admin')
+                ->groupBy('users.id', 'users.name', 'users.avatar', 'users.username', 'users.certifier', 'pings.id_user')
+                ->orderByRaw('CASE WHEN pings.id_user IS NOT NULL THEN 0 ELSE 1 END') // Met les "pings" en premier
+                ->orderByDesc('average_rating') // Ensuite, trie par note moyenne
+                ->orderByDesc('total_posts')
+                ->paginate(50);
+        } else {
+            $shopiners = User::select('users.id', 'users.name', 'users.avatar', 'users.username', 'users.certifier', DB::raw('AVG(etoiles) as average_rating'), DB::raw('COUNT(posts.id) as total_posts'))
+                ->leftJoin('ratings', 'users.id', '=', 'ratings.id_user_rated')
+                ->leftJoin('posts', 'users.id', '=', 'posts.id_user')
+                ->groupBy('users.id', 'users.name', 'users.avatar', 'users.username', 'users.certifier')
+                ->where('users.role', '!=', 'admin')
+                ->orderByDesc('average_rating')
+                ->orderByDesc('total_posts')
+                ->paginate(50);
+        }
 
 
 
