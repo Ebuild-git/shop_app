76a6c2d5265839b7d6f4d0e35f1c08947180d1f4
kg <48618359+kevingassam@users.noreply.github.com>
2024-04-10 12:31:58 +0200

    up

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/forget', function () {
 
 
 Route::get('/post/{id}', [ControllersHomeController::class, 'details_post'])->name('details_post');
-Route::get('/post/{id}/{titre}', [ControllersHomeController::class, 'details_post']);
+Route::get('/post/{id}/{titre}', [ControllersHomeController::class, 'details_post'])->name('details_post2');
 
 
 
