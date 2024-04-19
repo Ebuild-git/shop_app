49009aaa8257d46e273271a97bc830275e73e67b
kg <48618359+kevingassam@users.noreply.github.com>
2024-04-19 17:19:17 +0200

    up

--- resources/views/livewire/user/inscription.blade.php
+++ resources/views/livewire/user/inscription.blade.php
@@ -94,10 +94,19 @@
                         @endphp
                         <select wire:model="mois" class="form-control">
                             <option selected>Mois</option>
-                            @foreach (range(1, 12) as $m)
-                                <option value="{{ $m }}">{{ strftime('%B', mktime(0, 0, 0, $m, 1)) }}
-                                </option>
-                            @endforeach
+                            <option value="1">Janvier</option>
+                            <option value="2">Février</option>
+                            <option value="3">Mars</option>
+                            <option value="4">Avril</option>
+                            <option value="5">Mai</option>
+                            <option value="6">Juin</option>
+                            <option value="7">Juillet</option>
+                            <option value="8">Août</option>
+                            <option value="9">Septembre</option>
+                            <option value="10">Octobre</option>
+                            <option value="11">Novembre</option>
+                            <option value="12">Décembre</option>
+
                         </select>
                         <select wire:model="annee" class="form-control">
                             <option selected>Année</option>
