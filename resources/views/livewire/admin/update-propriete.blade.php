b70c63ed6a66413401727cc49c8f000c33d5bc3c
kg <48618359+kevingassam@users.noreply.github.com>
2024-04-19 21:36:09 +0200

    up

--- resources/views/livewire/admin/update-propriete.blade.php
+++ resources/views/livewire/admin/update-propriete.blade.php
@@ -1,6 +1,6 @@
 <form wire:submit="update">
     <div class="row ">
-        <div class="col-sm-8 ">
+        <div class="col-sm-4 ">
             <div>
                 @include('components.alert-livewire')
             </div>
@@ -52,25 +52,32 @@
                 @endif
             </div>
         </div>
-        <div class="col-sm-4">
+        <div class="col-sm-8">
             <hr>
             @if ($typeselected && $typeselected == 'option')
-                @foreach ($optionsCases as $key => $option)
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon1">
-                            <img width="20" height="20" src="https://img.icons8.com/fluency/48/checked-2.png"
-                                alt="checked-2" />
-                        </span>
-                        <input type="text" class="form-control" wire:model="optionsCases.{{ $key }}">
-                        <button class="btn btn-sm btn-light" type="button" wire:click="add_option()">
-                            <img width="20" height="20" src="https://img.icons8.com/fluency/48/add--v1.png"
-                                alt="add--v1" />
-                        </button>
-                        <button class="btn btn-sm btn-light" type="button" wire:click="delete_option({{ $key }})">
-                            <img width="20" height="20" src="https://img.icons8.com/fluency/20/delete-sign.png" alt="delete-sign"/>
-                        </button>
-                    </div>
-                @endforeach
+                <div class="row">
+                    @foreach ($optionsCases as $key => $option)
+                        <div class="col-sm-6">
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">
+                                    <img width="20" height="20"
+                                        src="https://img.icons8.com/fluency/48/checked-2.png" alt="checked-2" />
+                                </span>
+                                <input type="text" class="form-control"
+                                    wire:model="optionsCases.{{ $key }}">
+                                <button class="btn btn-sm btn-light" type="button" wire:click="add_option()">
+                                    <img width="20" height="20"
+                                        src="https://img.icons8.com/fluency/48/add--v1.png" alt="add--v1" />
+                                </button>
+                                <button class="btn btn-sm btn-light" type="button"
+                                    wire:click="delete_option({{ $key }})">
+                                    <img width="20" height="20"
+                                        src="https://img.icons8.com/fluency/20/delete-sign.png" alt="delete-sign" />
+                                </button>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             @endif
         </div>
     </div>
