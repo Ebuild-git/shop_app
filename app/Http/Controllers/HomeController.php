9c54cda72a7f3e5909262e61ebae0cec6a87e705
kg <48618359+kevingassam@users.noreply.github.com>
2024-04-21 15:40:12 +0200

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,13 +25,13 @@ class HomeController extends Controller
         $configuration = configurations::firstorCreate();
         $posts = posts::where('verified_at', '!=', null)->orderByDesc('created_at')->paginate(50);
         $last_post = posts::where('verified_at', '!=', null)
-            ->select("id", "titre", "photos", "old_achat", "prix", "id_sous_categorie", "statut")
+            ->select("id", "titre", "photos", "old_prix", "prix", "id_sous_categorie", "statut")
             ->orderByDesc('created_at')
             ->get();
         $luxurys = posts::join('sous_categories', 'posts.id_sous_categorie', '=', 'sous_categories.id')
             ->join('categories', 'sous_categories.id_categorie', '=', 'categories.id')
             ->where('categories.luxury', true)
-            ->select("posts.id", 'posts.old_achat', "posts.titre", "posts.photos", "posts.prix", "posts.statut", "posts.id_sous_categorie", "categories.id As id_categorie")
+            ->select("posts.id", 'posts.old_prix', "posts.titre", "posts.photos", "posts.prix", "posts.statut", "posts.id_sous_categorie", "categories.id As id_categorie")
             ->get();
         return view("User.index", compact("categories", "posts", "configuration", "last_post", "luxurys"));
     }
