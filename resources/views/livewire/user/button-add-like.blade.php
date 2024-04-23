704dbbc723a5701cdb84ccc0f093f48a4e124927
kg <48618359+kevingassam@users.noreply.github.com>
2024-04-23 15:46:29 +0200

    up

--- resources/views/livewire/user/button-add-like.blade.php
+++ resources/views/livewire/user/button-add-like.blade.php
@@ -3,15 +3,19 @@
         @if (Auth::id() != $post->id_user)
             <button class="btn custom-height btn-default btn-block mb-2 @if ($liked === true) btn-liked @endif "
                 type="button" wire:click=like()>
-                <span wire:loading>
-                    <i class="lni lni-heart mr-2"></i>
-                </span>
-                <span wire:loading.remove>
-                    <i class="lni lni-heart mr-2"></i>
-                    J'aime ( {{ $total }} )
-                </span>
-            </button>
+            @else
+                <button class="btn custom-height btn-default btn-block mb-2 btn-liked" type="button">
         @endif
+
+        <span wire:loading>
+            <i class="lni lni-heart mr-2"></i>
+        </span>
+        <span wire:loading.remove>
+            <i class="lni lni-heart mr-2"></i>
+            J'aime ( {{ $total }} )
+        </span>
+        </button>
+
     @endauth
     @guest
         <button class="btn custom-height btn-default btn-block mb-2 " type="button" data-toggle="modal" data-target="#login">
