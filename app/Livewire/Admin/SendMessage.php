d1a0a1f73ae4aa9bf734d4aa7c3db74dab03f7e0
Ebuild-git <achref.hamouda@e-build.tn>
2024-04-29 16:13:05 +0100

    up

--- app/Livewire/Admin/SendMessage.php
+++ app/Livewire/Admin/SendMessage.php
@@ -4,19 +4,20 @@ namespace App\Livewire\Admin;
 
 use App\Mail\SendMessage as MailSendMessage;
 use App\Models\configurations;
+use Exception;
 use Illuminate\Support\Facades\Mail;
 use Livewire\Component;
 
 class SendMessage extends Component
 {
-    public $email,$message,$sujet,$username;
+    public $email, $message, $sujet, $username;
 
     protected $listeners = ['sendDataUser'];
 
-    public function sendDataUser($email,$username)
+    public function sendDataUser($email, $username)
     {
-       $this->email =$email ;  
-       $this->username= $username; 
+        $this->email = $email;
+        $this->username = $username;
     }
 
     public function render()
@@ -24,14 +25,15 @@ class SendMessage extends Component
         return view('livewire.admin.send-message');
     }
 
-    public function envoyer(){
+    public function envoyer()
+    {
 
         $config = configurations::select("email_send_message")->first();
-        if(!$config){
+        if (!$config) {
             session()->flash('error', 'Une erreur est survenue !');
             return;
         }
-        if($config->email_send_message == ""){
+        if ($config->email_send_message == "") {
             session()->flash('error', "Veuillez configurer le mail d'expedition.");
             return;
         }
@@ -39,29 +41,32 @@ class SendMessage extends Component
 
         //validation
         $this->validate([
-            'email'=>'required|email|exists:users,email',
-            'message'=>'required|string',
-            'sujet'=>'required|string|max:200'
-        ],[
-            'email.exists'=>"Cet email n'est pas valide.",
+            'email' => 'required|email|exists:users,email',
+            'message' => 'required|string',
+            'sujet' => 'required|string|max:200'
+        ], [
+            'email.exists' => "Cet email n'est pas valide.",
             "required" =>  "Le champ :attribute est requis.",
             "string" =>   ":attribute doit être une chaîne de caractères.",
         ]);
 
-        $message =[
+        $message = [
             "destainataire" => $this->message,
             "message" => $this->message,
             "sujet" => $this->sujet,
             "email_send_message" => $config->email_send_message
         ];
 
-        //send message
-        Mail::to($this->email)->send(new MailSendMessage($message));
-
-        //flash success message en reset input
-        session()->flash("success", "Votre message a été envoyé avec succès.");
-        $this->sujet ="";
-        $this->message="";
-
+        try {
+            // Envoyer le message
+            Mail::to($this->email)->send(new MailSendMessage($message));
+            //flash success message en reset input
+            session()->flash("success", "Votre message a été envoyé avec succès.");
+            $this->sujet = "";
+            $this->message = "";
+        } catch (Exception $e) {
+            // Flash message d'erreur
+            session()->flash("error", "Une erreur s'est produite lors de l'envoi du message : " . $e->getMessage());
+        }
     }
 }
