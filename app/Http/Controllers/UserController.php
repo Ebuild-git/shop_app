6e1ea1d3029a0ee7931594da97e004608ba3b2ec
Ebuild-git <achref.hamouda@e-build.tn>
2024-06-05 13:58:14 +0100

    up

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,7 +46,7 @@ class UserController extends Controller
             foreach ($post->photos ?? [] as $img) {
                 Storage::disk('public')->delete($img);
             }
-            $post->delete();
+            $post->forceDelete();
             return response()->json(
                 [
                     'success' => true,
