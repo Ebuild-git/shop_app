405649b81de2bd59c126a701cfbe9029ed913be1
Ebuild-git <achref.hamouda@e-build.tn>
2024-07-03 14:57:37 +0100

    d

--- resources/views/components/sub-card-post.blade.php
+++ resources/views/components/sub-card-post.blade.php
@@ -22,7 +22,7 @@
         <div>
             <div class="text-left">
                 <h4 class="fw-bolder fs-md mb-0 lh-1 mb-1">
-                    <a href="/post/{{ $post->id }}" class="text-capitalize">
+                    <a href="/post/{{ $post->id }}/{{ Str::slug($post->titre) }}" class="text-capitalize">
                         {{ strtolower(  Str::limit($post->titre, 25) ) }}
                     </a>
                 </h4>
