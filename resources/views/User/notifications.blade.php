70e9906960228c28afa38942b73b3cce3d6d3481
Ebuild-git <achref.hamouda@e-build.tn>
2024-07-10 15:19:29 +0100

    s

--- resources/views/User/notifications.blade.php
+++ resources/views/User/notifications.blade.php
@@ -25,62 +25,60 @@
             <div class="col-sm-12">
                 <div class="card p-2">
                     @if ($notifications->count() > 0)
-                        <div class="text-end">
+                        <div class="text-end mb-2">
                             <button class="btn btn-sm btn-danger" onclick="delete_all_notification()">
                                 <i class="bi bi-x-lg "></i>
                                 Tout supprimer
                             </button>
                         </div>
                     @endif
-                    <table>
-                        @forelse ($notifications as $item)
-                            <tr class="border-bottom" id="tr-{{ $item->id }}">
-                                <td style="width: 60px;" class="my-auto">
+
+                    @forelse ($notifications as $item)
+                        <div class="card p-2 mb-1" id="tr-{{ $item->id }}">
+                            <div class=" d-flex justify-content-between" >
+                                <div style="width: 60px;" class="my-auto">
                                     <img width="45" height="45" src="/icons/alarm--v1.png" alt="alarm--v1" />
-                                </td>
-                                <td>
-                                    <h6>
-                                        <b>
-                                            @if ($item->url != null)
-                                                <a href="{{ $item->url }}" class="link">
-                                                @else
-                                                    <a href="#" class="link">
-                                            @endif
-                                            {{ $item->titre }}
-                                            </a>
-                                        </b>
-                                    </h6>
-                                    <i>{!! $item->message !!}</i>
-                                    <div style="text-align: right">
-                                        <div class="small">
-                                            <div>
-                                                <i class="bi bi-app-indicator"></i>
-                                                {{ \Carbon\Carbon::parse($item->created_at)->locale('fr')->diffForHumans() }}
-                                            </div>
-                                            <div>
-                                                <button type="button" class="btn btn-sm -btn-danger"
-                                                    onclick="delete_notification({{ $item->id }})">
-                                                    <i class="bi bi-trash3 text-danger"></i> Supprimer
-                                                </button>
+                                </div>
+                                <div class="w-100">
+                                    <div class="d-flex justify-content-between">
+                                        <h6>
+                                            <b>
+                                                @if ($item->url != null)
+                                                    <a href="{{ $item->url }}" class="link">
+                                                    @else
+                                                        <a href="#" class="link">
+                                                @endif
+                                                {{ $item->titre }}
+                                                </a>
+                                            </b>
+                                        </h6>
+                                        <div style="text-align: right">
+                                            <div class="small">
+                                                <div>
+                                                    <i class="bi bi-app-indicator"></i>
+                                                    {{ \Carbon\Carbon::parse($item->created_at)->locale('fr')->diffForHumans() }}
+                                                    &nbsp;
+                                                    <span onclick="delete_notification({{ $item->id }})" class="cusor">
+                                                        <i class="bi bi-trash3 text-danger"></i>
+                                                    </span>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </td>
-                                <td>
-
-                                </td>
-                            </tr>
-                        @empty
-                            <div class="pb-5 pt-5 text-center">
-                                <img src="https://static.vecteezy.com/system/resources/thumbnails/023/570/826/small/still-empty-no-notification-yet-concept-illustration-line-icon-design-eps10-modern-graphic-element-for-landing-page-empty-state-ui-infographic-vector.jpg"
-                                    alt="">
-                                <h6 class="text-center">No Notifications</h6>
-                                <span class="text-muted">
-                                    <i> vous n'avez pas de notification actuellement.</i>
-                                </span>
+                                    <p>{!! $item->message !!}</p>
+                                </div>
                             </div>
-                        @endforelse
-                    </table>
+                        </div>
+                    @empty
+                        <div class="pb-5 pt-5 text-center">
+                            <img src="https://static.vecteezy.com/system/resources/thumbnails/023/570/826/small/still-empty-no-notification-yet-concept-illustration-line-icon-design-eps10-modern-graphic-element-for-landing-page-empty-state-ui-infographic-vector.jpg"
+                                alt="">
+                            <h6 class="text-center">Pas Notifications</h6>
+                            <span class="text-muted">
+                                <i> vous n'avez pas de notification actuellement.</i>
+                            </span>
+                        </div>
+                    @endforelse
                 </div>
             </div>
         </div>
