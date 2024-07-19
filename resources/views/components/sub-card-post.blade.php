c5a219009e150880a7f6bf91194186e52a857f01
Ebuild-git <achref.hamouda@e-build.tn>
2024-07-19 14:38:47 +0100

    d

--- resources/views/components/sub-card-post.blade.php
+++ resources/views/components/sub-card-post.blade.php
@@ -1,51 +1,53 @@
 <div class="card-footer b-0 p-0 pt-2 bg-white">
     @if ($show)
-        <div class="d-flex justify-content-between mb-1" style="font-size: 14px;">
-            <div class="d-flex justify-content-between mb-1">
-                @if ($post->changements_prix->count())
-                    <strike class="elis_rty color strong">
-                        {{ $post->getOldPrix() }} <sup>DH</sup>
-                    </strike>
-                    &nbsp;&nbsp;&nbsp;
-                @endif
-                <div class="@if ($post->changements_prix->count()) text-danger @else color @endif ft-bold fs-sm">
-                    {{ $post->getPrix() }} <sup>DH</sup>
-                </div>
-            </div>
-            @if ($post->sous_categorie_info->categorie->luxury == 1)
-                <div class="color strong">
-                    <i class="bi bi-gem"></i>
-                    LUXURY
-                </div>
+    <div class="d-flex justify-content-between mb-1" style="font-size: 14px;">
+        <div class="d-flex justify-content-between mb-1">
+            @if ($post->changements_prix->count())
+            <strike class="elis_rty color strong">
+                {{ $post->getOldPrix() }} <sup>DH</sup>
+            </strike>
+            &nbsp;&nbsp;&nbsp;
             @endif
+            <div class="@if ($post->changements_prix->count()) text-danger @else color @endif ft-bold fs-sm">
+                {{ $post->getPrix() }} <sup>DH</sup>
+            </div>
         </div>
-        <div>
-            <div class="text-left">
-                <h4 class="fw-bolder fs-md mb-0 lh-1 mb-1">
-                    <a href="/post/{{ $post->id }}/{{ Str::slug($post->titre) }}" class="text-capitalize">
-                        {{ strtolower(Str::limit($post->titre, 25)) }}
-                    </a>
-                </h4>
-                <div class="d-flex justify-content-between">
-                    <div>
-                        {{ $post->sous_categorie_info->titre }}
-                    </div>
-                    @if ($post->proprietes)
-                        <div class="strong">
-                            @if ($post->proprietes['Taille'] ?? null)
-                                {{ $post->proprietes['Taille'] }}
-                            @elseif($post->proprietes['Pointure'] ?? null)
-                                {{ $post->proprietes['Pointure'] }}
-                            @elseif($post->proprietes['Pointure - Bébé'] ?? null)
-                                {{ $post->proprietes['Pointure - Bébé'] }}
-                            @elseif($post->proprietes['Taille - bébé'] ?? null)
-                                {{ $post->proprietes['Taille - bébé'] }}
-                            @else
-                            @endif
-                        </div>
+        @if ($post->sous_categorie_info->categorie->luxury == 1)
+        <div class="color strong">
+            <i class="bi bi-gem"></i>
+            LUXURY
+        </div>
+        @endif
+    </div>
+    <div>
+        <div class="text-left">
+            <h4 class="fw-bolder fs-md mb-0 lh-1 mb-1">
+                <a href="/post/{{ $post->id }}/{{ Str::slug($post->titre) }}" class="text-capitalize">
+                    {{ strtolower(Str::limit($post->titre, 25)) }}
+                </a>
+            </h4>
+            <div class="d-flex justify-content-between">
+                <div>
+                    {{ $post->sous_categorie_info->titre }}
+                </div>
+                @if ($post->proprietes)
+                <div class="strong">
+                    @if ($post->proprietes['Taille'] ?? null)
+                    {{ $post->proprietes['Taille'] }}
+                    @elseif($post->proprietes['Pointure'] ?? null)
+                    {{ $post->proprietes['Pointure'] }}
+                    @elseif($post->proprietes['Pointure - Bébé'] ?? null)
+                    {{ $post->proprietes['Pointure - Bébé'] }}
+                    @elseif($post->proprietes['Pointure- Bébé'] ?? null)
+                    {{ $post->proprietes['Pointure- Bébé'] }}
+                    @elseif($post->proprietes['Taille - bébé'] ?? null)
+                    {{ $post->proprietes['Taille - bébé'] }}
+                    @else
                     @endif
                 </div>
+                @endif
             </div>
         </div>
+    </div>
     @endif
 </div>
