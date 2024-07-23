be8915f43f77857f35c258f4600ce4e7cf7018f8
Ebuild-git <achref.hamouda@e-build.tn>
2024-07-23 12:48:57 +0100

    d

--- resources/views/livewire/user/rating.blade.php
+++ resources/views/livewire/user/rating.blade.php
@@ -1,37 +1,41 @@
 <div>
     @if ($can_rate)
-    <div>
-        @if ($last_buy)
-        <div class="alert alert-info">
-            <i class="bi bi-bag-check"></i>
-            Votre achat le plus récent chez <b>{{ $user->username }}</b> date du : {{ $last_buy->sell_at}}
-            <div>
-                <b>
-                    <i class="bi bi-arrow-right"></i> {{ Str::limit( $last_buy->titre , 40)}}
-                </b>
-            </div>
-        </div>
-        @endif
-        <div class="d-flex justify-content-between">
-            @for ($i = 0; $i < 5; $i++) <button type="button" class="w-100 btn-note-rating"
-                wire:click='rate({{ $i+1 }})'>
-                <i class="bi bi-star-fill"></i> {{ $i+1 }}
-                </button>
+        <div>
+            @if ($last_buy)
+                <div class="alert alert-info">
+                    <i class="bi bi-bag-check"></i>
+                    Votre achat le plus récent chez <b>{{ $user->username }}</b> date du : {{ $last_buy->sell_at }}
+                    <div>
+                        <b>
+                            <i class="bi bi-arrow-right"></i> {{ Str::limit($last_buy->titre, 40) }}
+                        </b>
+                    </div>
+                </div>
+            @endif
+            <div class="d-flex justify-content-between">
+                @for ($i = 0; $i < 5; $i++)
+                    <button type="button" class="w-100 btn-note-rating" wire:click='rate({{ $i + 1 }})'>
+                        <i class="bi bi-star-fill"></i> {{ $i + 1 }}
+                    </button>
                 @endfor
 
+            </div>
         </div>
-    </div>
     @else
-    <div class="text-center p-3 alert-danger">
-        <b class="h5 text-danger">
-            Attention !
-        </b>
-        <p>
-            Vous pouvez laisser un avis sous forme d'étoiles uniquement si vous avez effectué un achat auprès de ce
-            vendeur !
-        </p>
-    </div>
+        <div class="text-center p-3 alert-danger">
+            <b class="h5 text-danger">
+                Attention !
+            </b>
+            <p>
+                Vous pouvez laisser un avis sous forme d'étoiles uniquement si vous avez effectué un achat auprès de ce
+                vendeur !
+            </p>
+        </div>
     @endif
     @include('components.alert-livewire')
-
+    <div class="text-center">
+        <span wire:loading>
+            <x-Loading></x-Loading>
+        </span>
+    </div>
 </div>
