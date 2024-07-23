955287e5bdf63a62482ec5cf1043ca06288b3466
Ebuild-git <achref.hamouda@e-build.tn>
2024-07-23 13:06:32 +0100

    s

--- app/Livewire/User/Rating.php
+++ app/Livewire/User/Rating.php
@@ -11,48 +11,50 @@ use Livewire\Component;
 
 class Rating extends Component
 {
-    public $id_user,$user,$last_buy,$rate;
-    public $can_rate = false;
+    public $id_user, $user, $last_buy, $rate;
+    public $can_rate = true;
 
     public function mount($id_user)
     {
         $this->id_user = $id_user;
         $this->user = User::find($id_user);
         $this->last_buy = posts::where('id_user', $id_user)
-        ->where('id_user_buy',Auth::id())
-        ->whereIn('statut', ['livré', 'vendu'])
-        ->Orderby("sell_at","Desc")
-        ->first();
-        if($this->last_buy ){
-            $this->rate = ratings::where('id_user_buy', Auth::user()->id)
-            ->where("id_user_sell", $this->user->id)
-            ->where('id_post',$this->last_buy->id)
-            ->Orderby("created_at","Desc")
+            ->where('id_user_buy', Auth::id())
+            ->whereIn('statut', ['livré', 'vendu'])
+            ->Orderby("sell_at", "Desc")
             ->first();
+        if (!$this->last_buy) {
+            $this->can_rate = false;
+            return;
         }
-        
+
+        $this->rate = ratings::where('id_user_buy', Auth::user()->id)
+            ->where("id_user_sell", $this->user->id)
+            ->where('id_post', $this->last_buy->id)
+            ->Orderby("created_at", "Desc")
+            ->first();
+        if ($this->rate) {
+            $this->can_rate = false;
+            return;
+        }
+
+        $date = Carbon::now();
+        $date = $date->subDays(14);
+        if ($this->last_buy->sell_at > $date) {
+            $this->can_rate = false;
+        }
     }
 
     public function render()
     {
 
-        $date = Carbon::now();
-        $date = $date->subDays(14);
-        if($this->last_buy){
-            if($this->last_buy->sell_at > $date) {
-                if(!$this->rate){
-                     $this->can_rate = true;
-                }
-            }
-        }
-        
 
 
         return view('livewire.user.rating');
     }
 
     public function rate($value)
-    { 
+    {
         $value = intval($value);
         //verified $value est between 1 and 5 and it's integer
         if (!isset($value) || ($value < 1 || $value > 5)) {
@@ -61,28 +63,24 @@ class Rating extends Component
         }
 
 
-        if(!$this->last_buy){
+        if (!$this->last_buy) {
             session()->flash("error", "Vous ne pouvez laisser un avis sous forme d'étoiles uniquement si vous avez effectué un achat auprès de ce vendeur!");
             return;
         }
 
 
 
-    
-
-         
-
         // veridier que il n'a pas encore donner une note
         if ($this->rate) {
             $this->dispatch('alert', ['message' => "Vous ne pouvez pas modifier votre avis!", 'type' => 'warning']);
             return;
-        } 
+        }
 
         //on verifie que sa fais moins de 2 semaine qu'il a fait cet achat
         $date = Carbon::now();
         $date = $date->subDays(14);
         if ($this->last_buy->sell_at < $date) {
-            session()->flash("error","Vous ne pouvez pas donner une note plus de 2 semaine après votre achat!");
+            session()->flash("error", "Vous ne pouvez pas donner une note plus de 2 semaine après votre achat!");
             return;
         }
 
@@ -90,10 +88,9 @@ class Rating extends Component
         $rate = new ratings();
         $rate->id_user_buy = Auth::user()->id;
         $rate->id_user_sell = $this->user->id;
-        $rate->id_post = $this->last_buy->id; 
+        $rate->id_post = $this->last_buy->id;
         $rate->etoiles = $value;
         $rate->save();
         $this->dispatch('alert', ['message' => "Votre note a été enregistré !", 'type' => 'success']);
-        
     }
 }
--- resources/views/livewire/user/rating.blade.php
+++ resources/views/livewire/user/rating.blade.php
@@ -1,36 +1,48 @@
 <div>
-    @if ($can_rate)
+    @if ($last_buy)
+    <div class="alert alert-info">
+        <i class="bi bi-bag-check"></i>
+        Votre achat le plus récent chez <b>{{ $user->username }}</b> date du : {{ $last_buy->sell_at }}
         <div>
-            @if ($last_buy)
-                <div class="alert alert-info">
-                    <i class="bi bi-bag-check"></i>
-                    Votre achat le plus récent chez <b>{{ $user->username }}</b> date du : {{ $last_buy->sell_at }}
-                    <div>
-                        <b>
-                            <i class="bi bi-arrow-right"></i> {{ Str::limit($last_buy->titre, 40) }}
-                        </b>
-                    </div>
-                </div>
-            @endif
-            <div class="d-flex justify-content-between">
-                @for ($i = 0; $i < 5; $i++)
-                    <button type="button" class="w-100 btn-note-rating" wire:click='rate({{ $i + 1 }})'>
-                        <i class="bi bi-star-fill"></i> {{ $i + 1 }}
-                    </button>
+            <b>
+                <i class="bi bi-arrow-right"></i> {{ Str::limit($last_buy->titre, 40) }}
+            </b> <br>
+            @if ($rate)
+            <i class="bi bi-arrow-right"></i> Note : {{ $rate->etoiles }} / 5
+            @for ($i = 0; $i < $rate->etoiles; $i++)
+                <i class="bi bi-star-fill"></i>
+                @endfor
+                @endif
+        </div>
+    </div>
+    @endif
+    @if ($can_rate)
+    <div>
+        <div class="d-flex justify-content-between">
+            @for ($i = 0; $i < 5; $i++) <button type="button" class="w-100 btn-note-rating"
+                wire:click='rate({{ $i + 1 }})'>
+                <i class="bi bi-star-fill"></i> {{ $i + 1 }}
+                </button>
                 @endfor
 
-            </div>
         </div>
+    </div>
     @else
-        <div class="text-center p-3 alert-danger">
-            <b class="h5 text-danger">
-                Attention !
-            </b>
-            <p>
-                Vous pouvez laisser un avis sous forme d'étoiles uniquement si vous avez effectué un achat auprès de ce
-                vendeur !
-            </p>
-        </div>
+    <div class="p-3 alert-danger">
+        <b class="h5 text-danger">
+            Attention !
+        </b>
+        @if (!$rate)
+        <p>
+            Vous pouvez laisser un avis sous forme d'étoiles uniquement si vous avez effectué un achat auprès de ce
+            vendeur !
+        </p>
+        @else
+        <p>
+            Vous pouvez ne pouvez plus laisser un avis car vous l'avez déja fait !
+        </p>
+        @endif
+    </div>
     @endif
     @include('components.alert-livewire')
     <div class="text-center">
