bd88c48e8bead4b3f9790f6f307ef46fdb5963bb
hazarnenni <hazarne14@gmail.com>
2024-08-06 00:54:35 +0100

    fix-sticky-table-head

--- resources/views/components/Liste-mes-achats.blade.php
+++ resources/views/components/Liste-mes-achats.blade.php
@@ -1,65 +1,69 @@
-<table class="table">
-    <thead style="background-color: #008080;color: white !important;">
-        <tr>
-            <th scope="col" style="width: 51px;"></th>
-            <th scope="col">Nom de l'article</th>
-            <th scope="col">Date d'achat</th>
-            <th scope="col">Prix d'achat</th>
-            <th scope="col">Shopiner</th>
-            <th scope="col" class="text-end">Satut de l'xpedition</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse ($achats as $achat)
-            <tr>
-                <td style="width: 41px;">
-                    <div class="avatar-small-product">
-                        <img src="{{ Storage::url($achat->photos[0] ?? '') }}" alt="avtar">
-                    </div>
-                </td>
-                <td>
-                    <a href="/post/{{ $achat->id }}" class="link h6"> {{ $achat->titre }} </a>
-                </td>
-                <td>
-                    {{  \Carbon\Carbon::parse($achat->sell_at)->format("d-m-Y") }}
-                </td>
-                <td>
-                    <span class="strong color">
-                        <i class="bi bi-tag"></i>
-                        {{ $achat->getPrix() }}
-                        DH
-                    </span>
-                </td>
-                <td>
-                    @if ($achat->user_info)
-                        <a href="{{ route('user_profile', ['id' => $achat->user_info->id]) }}">
-                            {{ $achat->user_info->username }}
-                        </a>
-                    @endif
-                </td>
-                <td class="text-end">
-                    <x-StatutLivraison :statut="$achat->statut"></x-StatutLivraison>
-                </td>
-            </tr>
-        @empty
-            <tr>
-                <td colspan="6">
-                    <div class="alert alert-info text-center">
-                        <div>
-                            <img width="100" height="100"
-                                src="https://img.icons8.com/carbon-copy/100/737373/shopping-cart-loaded.png"
-                                alt="shopping-cart-loaded" />
-                        </div>
-                        <h6 class="text-center">Aucun Achat !</h6>
-                        <span class="text-muted">
-                            <i> 
-                                vous n'avez pas d'achat actuellement .
-                            </i>
-                        </span>
-                    </div>
-                </td>
-            </tr>
-        @endforelse
-    </tbody>
-</table>
+<div id="table-wrapper">
+    <div id="table-scroll">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" style="width: 51px;"></th>
+                    <th scope="col">Nom de l'article</th>
+                    <th scope="col">Date d'achat</th>
+                    <th scope="col">Prix d'achat</th>
+                    <th scope="col">Shopiner</th>
+                    <th scope="col" class="text-end">Satut de l'xpedition</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($achats as $achat)
+                    <tr>
+                        <td style="width: 41px;">
+                            <div class="avatar-small-product">
+                                <img src="{{ Storage::url($achat->photos[0] ?? '') }}" alt="avtar">
+                            </div>
+                        </td>
+                        <td>
+                            <a href="/post/{{ $achat->id }}" class="link h6"> {{ $achat->titre }} </a>
+                        </td>
+                        <td>
+                            {{  \Carbon\Carbon::parse($achat->sell_at)->format("d-m-Y") }}
+                        </td>
+                        <td>
+                            <span class="strong color">
+                                <i class="bi bi-tag"></i>
+                                {{ $achat->getPrix() }}
+                                DH
+                            </span>
+                        </td>
+                        <td>
+                            @if ($achat->user_info)
+                                <a href="{{ route('user_profile', ['id' => $achat->user_info->id]) }}">
+                                    {{ $achat->user_info->username }}
+                                </a>
+                            @endif
+                        </td>
+                        <td class="text-end">
+                            <x-StatutLivraison :statut="$achat->statut"></x-StatutLivraison>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="6">
+                            <div class="alert alert-info text-center">
+                                <div>
+                                    <img width="100" height="100"
+                                        src="https://img.icons8.com/carbon-copy/100/737373/shopping-cart-loaded.png"
+                                        alt="shopping-cart-loaded" />
+                                </div>
+                                <h6 class="text-center">Aucun Achat !</h6>
+                                <span class="text-muted">
+                                    <i>
+                                        vous n'avez pas d'achat actuellement .
+                                    </i>
+                                </span>
+                            </div>
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+</div>
 {{ $achats->links('pagination::bootstrap-4') }}
