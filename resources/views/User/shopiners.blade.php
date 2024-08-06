549978bd1413ccb2b8e3166c0b87967a92fcff85
hazarnenni <hazarne14@gmail.com>
2024-08-06 12:31:07 +0100

    add-rating-filter

--- resources/views/User/shopiners.blade.php
+++ resources/views/User/shopiners.blade.php
@@ -39,3 +39,36 @@
 
 
 @endsection
+<style>
+    .custom-select {
+    background-color: #fff;
+    border: 1px solid #ced4da;
+    border-radius: 0.25rem;
+    padding: 0.375rem 1.75rem 0.375rem 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    color: #495057;
+    background-clip: padding-box;
+    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    appearance: none;
+    position: relative;
+}
+
+.custom-select::after {
+    content: "\25BC";
+    position: absolute;
+    top: 50%;
+    right: 0.75rem;
+    pointer-events: none;
+    transform: translateY(-50%);
+    font-size: 1rem;
+    color: #495057;
+}
+
+.custom-select:hover, .custom-select:focus {
+    border-color: #80bdff;
+    outline: 0;
+    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+}
+
+</style>
