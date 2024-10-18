0ea6eb6cca170d85a1ae93060e7ce7355880f46c
hazarnenni <hazarne14@gmail.com>
2024-10-18 13:03:24 +0100

    fix-email-design

--- resources/views/Mails/Message.blade.php
+++ resources/views/Mails/Message.blade.php
@@ -32,62 +32,36 @@
 
 </head>
 
-<body class="pt-5 fw-light" style="background-color: #f4f8fb;">
+<body>
 
-    <div class="container">
-        <div class="text-center">
+    <div class="email-container">
+        <div class="email-header">
             <img src="{{ config('app.url') }}/icons/logo.png" height="20" alt="Logo" />
         </div>
-
-        <div class="text-center mt-3">
-            <h3 class="color">{{ config('app.name') }}</h3>
-        </div>
         <br>
-        <div class="row">
-            <div class="col-sm-8 mx-auto bg-white p-3">
-                {{-- <p>
-                    Bonjour {{ $user->username }}, <br>
-                </p> --}}
+        <div class="email-content">
                 <p>
                     Vous avez reçu un nouveau message avec le sujet suivant : <strong>{{$data['sujet']}}</strong>
                 </p>
                 <p>
-                    <strong>Message :</strong><br>
+                    <strong>Message:</strong><br>
                     {{$data['message']}}
                 </p>
 
-                <p>
-                    Merci de votre attention !
-                    <br>
-                    L’équipe de <strong style="color:black;">{{ config('app.name') }}</strong>
-                </p>
-            </div>
+                <span>Merci de votre attention !</span>
+                <br>
+                <p>L’équipe de  <strong style="color:black;">SHOP</strong><span style="color:#008080;"><strong>IN</strong></span></p>
         </div>
     </div>
     <br><br>
-    <div class="footer p-2 text-center">
-        {{ config('app.name') }}
-        <br>
-        <div class="small">
-            Suivez-nous sur nos réseaux sociaux !
-        </div>
-        <div class="mb-5 mt-2">
-            <a href="#">
-                <img width="30" height="30" src="https://img.icons8.com/glyph-neue/30/FFFFFF/facebook-new.png"
-                    alt="facebook-new" />
-            </a>
-            <a href="#">
-                <img width="30" height="30"
-                    src="https://img.icons8.com/ios-filled/30/FFFFFF/instagram-new--v1.png" alt="instagram-new--v1" />
-            </a>
-            <a href="#">
-                <img width="30" height="30"
-                    src="https://img.icons8.com/glyph-neue/30/FFFFFF/linkedin-circled.png" alt="linkedin-circled" />
-            </a>
-            <a href="#">
-                <img width="30" height="30" src="https://img.icons8.com/ios-filled/30/FFFFFF/tiktok--v1.png"
-                    alt="tiktok--v1" />
-            </a>
+    <div class="email-footer">
+        <p>{{ config('app.name') }}</p>
+        <p class="small">Suivez-nous sur nos réseaux sociaux !</p>
+        <div class="social-icons">
+            <a href="#"><img src="https://img.icons8.com/glyph-neue/30/FFFFFF/facebook-new.png" alt="Facebook"></a>
+            <a href="#"><img src="https://img.icons8.com/ios-filled/30/FFFFFF/instagram-new--v1.png" alt="Instagram"></a>
+            <a href="#"><img src="https://img.icons8.com/glyph-neue/30/FFFFFF/linkedin-circled.png" alt="LinkedIn"></a>
+            <a href="#"><img src="https://img.icons8.com/ios-filled/30/FFFFFF/tiktok--v1.png" alt="TikTok"></a>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
