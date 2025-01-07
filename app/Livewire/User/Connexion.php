b6625c51008f3499b04d0de98f6d7fd244c607d4
hazarnenni <hazarne14@gmail.com>
2025-01-07 19:16:52 +0100

    modify-msg

--- app/Livewire/User/Connexion.php
+++ app/Livewire/User/Connexion.php
@@ -64,7 +64,7 @@ class Connexion extends Component
         }
 
         if ($user->locked == true) {
-            session()->flash("error", "Compte bloqué. Veuillez contacter un administrateur pour réactiver votre compte.");
+            session()->flash("error", "Compte bloqué. Veuillez <a href='/contact'>cliquer ici</a> pour contacter un administrateur et réactiver votre compte.");
             return;
         }
         $user->update(['last_login_at' => now()]);
