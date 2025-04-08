45e1ca8a5c8675127d8cc85f30fb3e44d6c88c7b
hazarnenni <hazarne14@gmail.com>
2025-04-08 16:32:03 +0100

    fix-en-trans-file

--- resources/views/livewire/user/mode-toggle.blade.php
+++ resources/views/livewire/user/mode-toggle.blade.php
@@ -1,6 +1,6 @@
 <div>
     <li class="mode-toggle">
-        <span class="medium text-medium">Mode voyage</span>
+        <span class="medium text-medium">{{ __('Mode voyage') }}</span>
         <label class="switch" style="margin-right: 5px; margin-top: 10px;">
             <input type="checkbox" wire:click="toggleVoyageMode" {{ $isVoyageMode ? 'checked' : '' }}>
             <span class="slider round"></span>
