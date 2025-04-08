00ce98de8cdcf9ebf41757183d0ed7b959dfcede
hazarnenni <hazarne14@gmail.com>
2025-04-08 13:46:31 +0100

    report-trans

--- resources/views/livewire/user/signalement.blade.php
+++ resources/views/livewire/user/signalement.blade.php
@@ -4,49 +4,49 @@
             <img src="/icons/bouclier.svg" alt="icon alert" class="icon-modern">
             <br>
             <b class="text-success text-modern">
-                Merci pour votre signalement!
+                {{ __('Thank you for your report!')}}
             </b>
             <p class="text-muted text-modern">
-                Nous allons examiner cette annonce dans les plus brefs délais.
+                {{ __('We will review this ad as soon as possible.')}}
             </p>
         </div>
     @else
         <div class="text-center mb-4">
             <h1 class="m-0 ft-regular h5 text-danger">
                 <i class="bi bi-exclamation-octagon"></i>
-                Signaler cet annonce
+                {{ __('report_ad') }}
             </h1>
             <span class="text-muted">
                 "
                 <span class="color text-capitalize">
-                    {{ $post->titre }}
+                    {{ \App\Traits\TranslateTrait::TranslateText($post->titre) }}
                 </span>
                 "
             </span>
         </div>
         <form wire:submit="signaler">
-            <b>Motif</b>
+            <b>{{ __('reason') }}</b>
             <select required wire:model="type" class="form-control shadow-none">
-                <option value="">Choisir un motif</option>
-                <option value="Annonce de produits interdits ou illégaux">Annonce de produits interdits ou illégaux</option>
-                <option value="Annonce multiple du même article">Annonce multiple du même article</option>
-                <option value="Autres violations des politiques du site">Autres violations des politiques du site</option>
-                <option value="Contenu inapproprié">Contenu inapproprié</option>
-                <option value="Description trompeuse de l'état de l'article">Description trompeuse de l'état de l'article</option>
-                <option value="Fraude ou activité suspecte">Fraude ou activité suspecte</option>
-                <option value="Information incorrecte sur la taille, la couleur, etc.">Information incorrecte sur la taille, la couleur, etc.</option>
-                <option value="Photos floues ou de mauvaise qualité">Photos floues ou de mauvaise qualité</option>
-                <option value="Prix excessif pour le produit mis en vente">Prix excessif pour le produit mis en vente</option>
-                <option value="Produit contrefait ou non authentique">Produit contrefait ou non authentique</option>
-                <option value="Publicité non autorisée ou spam">Publicité non autorisée ou spam</option>
-                <option value="Violation des droits d'auteur ou de la propriété intellectuelle">Violation des droits d'auteur ou de la propriété intellectuelle</option>
+                <option value="">{{ __('choose_reason') }}</option>
+                <option value="Annonce de produits interdits ou illégaux">{{ __('ad_illegal_products') }}</option>
+                <option value="Annonce multiple du même article">{{ __('multiple_ads_same_item') }}</option>
+                <option value="Autres violations des politiques du site">{{ __('other_policy_violations') }}</option>
+                <option value="Contenu inapproprié">{{ __('inappropriate_content') }}</option>
+                <option value="Description trompeuse de l'état de l'article">{{ __('misleading_description') }}</option>
+                <option value="Fraude ou activité suspecte">{{ __('fraud_suspicious_activity') }}</option>
+                <option value="Information incorrecte sur la taille, la couleur, etc.">{{ __('incorrect_info') }}</option>
+                <option value="Photos floues ou de mauvaise qualité">{{ __('blurry_photos') }}</option>
+                <option value="Prix excessif pour le produit mis en vente">{{ __('excessive_price') }}</option>
+                <option value="Produit contrefait ou non authentique">{{ __('counterfeit_product') }}</option>
+                <option value="Publicité non autorisée ou spam">{{ __('unauthorized_advertisement') }}</option>
+                <option value="Violation des droits d'auteur ou de la propriété intellectuelle">{{ __('copyright_violation') }}</option>
             </select>
             @error('type')
                 <small class="form-text text-danger">{{ $message }}</small>
             @enderror
-            <b>Message</b>
+            <b>{{ \App\Traits\TranslateTrait::TranslateText('Message') }}</b>
             <textarea wire:model="message" class="form-control border-r shadow-none" rows="6"
-            placeholder="Veuillez entrer un message expliquant clairement la raison de votre signalement. Votre message doit contenir au moins 20 caractères."
+            placeholder="{{ __('message_placeholder') }}"
             oninput="updateCharCount(event)"
             ></textarea>
             @error('message')
@@ -54,13 +54,13 @@
             @enderror
             <div class="modal-footer d-flex justify-content-between align-items-center">
                 <span id="charCount" class="text-muted">
-                    {{ strlen($message) }} caractères
+                    {{ strlen($message) }} {{ __('caractères') }}
                 </span>
                 <button type="submit" class="btn btn-sm btn-danger">
                     <span wire:loading>
                         <x-Loading></x-Loading>
                     </span>
-                    Envoyer le signalement
+                    {{ __('send_report') }}
                     <i class="bi bi-arrow-right-circle-fill"></i>
                 </button>
             </div>
@@ -70,8 +70,9 @@
 </div>
 <script>
     function updateCharCount(event) {
+        var characterLabel = @json(__('caractères'));
         const charCount = event.target.value.length;
-        document.getElementById('charCount').textContent = charCount + ' caractères';
+        document.getElementById('charCount').textContent = charCount + ' ' + characterLabel;
     }
 </script>
 <script>
