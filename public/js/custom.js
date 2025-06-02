76347aa5ae9f949e5a309abe6e7317cee2bcc02c
hazarnenni <hazarne14@gmail.com>
2025-06-02 17:55:07 +0100

    fix-zoom

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,7 +1,8 @@
 function zoom(e) {
     var zoomer = e.currentTarget;
     e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
-    e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+    e.offsetY ? offsetY = e.offsetY : offsetY = e.touches[0].pageY;
+
     x = offsetX / zoomer.offsetWidth * 100
     y = offsetY / zoomer.offsetHeight * 100
     zoomer.style.backgroundPosition = x + '% ' + y + '%';
