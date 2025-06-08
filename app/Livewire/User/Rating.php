883d9db69d4b4dc7c3ed71da900e59abca276a24
hazarnenni <hazarne14@gmail.com>
2025-06-08 16:21:59 +0100

    fix-trans-rating

--- app/Livewire/User/Rating.php
+++ app/Livewire/User/Rating.php
@@ -53,44 +53,39 @@ class Rating extends Component
             ->first();
 
         $value = intval($value);
-        //verified $value est between 1 and 5 and it's integer
+
         if (!isset($value) || ($value < 1 || $value > 5)) {
-            session()->flash("error", "Vous devez donner une note entre 1 et 5");
+            session()->flash("error", __("error.rating_required"));
             return;
         }
 
 
         if (!$this->last_buy) {
-            session()->flash("error", "Vous ne pouvez laisser un avis sous forme d'étoiles uniquement si vous avez effectué un achat auprès de ce vendeur!");
+            session()->flash("error", __("error.no_purchase"));
             return;
         }
 
 
-
-        // veridier que il n'a pas encore donner une note
         if ($old_rate) {
-            $this->dispatch('alert', ['message' => "Vous ne pouvez pas modifier votre avis!", 'type' => 'warning']);
+            $this->dispatch('alert', ['message' => __("error.already_rated"), 'type' => 'warning']);
             return;
         }
 
-        //on verifie que sa fais moins de 2 semaine qu'il a fait cet achat
         $date = Carbon::now();
         $date = $date->subDays(14);
         if ($this->last_buy->sell_at < $date) {
-            session()->flash("error", "Vous ne pouvez pas donner une note plus de 2 semaine après votre achat!");
+            session()->flash("error", __("error.too_late"));
             return;
         }
 
-        //tout est ok on enregistre la note
         $rate = new ratings();
         $rate->id_user_buy = Auth::user()->id;
         $rate->id_user_sell = $this->user->id;
         $rate->id_post = $this->last_buy->id;
         $rate->etoiles = $value;
         $rate->save();
-        $this->dispatch('alert', ['message' => "Votre note a été enregistré !", 'type' => 'success']);
+        $this->dispatch('alert', ['message' => __("success.rating_saved"), 'type' => 'success']);
 
-        //refresh 
         $this->mount($this->id_user);
     }
 }
