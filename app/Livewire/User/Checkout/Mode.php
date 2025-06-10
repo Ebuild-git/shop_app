9091f374198a7aae75323d092c9e80b12638afa1
hazarnenni <hazarne14@gmail.com>
2025-06-10 17:25:54 +0100

    rm-locale

--- app/Livewire/User/Checkout/Mode.php
+++ app/Livewire/User/Checkout/Mode.php
@@ -375,9 +375,6 @@ class Mode extends Component
                 logger("❌ Failed to send email to: {$seller->email}. Error: " . $e->getMessage());
             }
 
-            $originalLocale = app()->getLocale();
-            app()->setLocale($seller->locale ?? 'fr');
-
             $articlesLinks = $posts->map(function ($post) {
                 $url = route('details_post2', ['id' => $post->id, 'titre' => $post->titre]);
                 return "<a href='{$url}' class='underlined-link'>" . e($post->titre) . "</a>";
@@ -402,7 +399,6 @@ class Mode extends Component
             ]);
             $notification->save();
             event(new UserEvent($seller->id));
-            app()->setLocale($originalLocale);
         }
 
 
