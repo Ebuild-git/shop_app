2ae8106079ce718ca6f3666064722f0d60278112
hazarnenni <hazarne14@gmail.com>
2026-01-27 16:09:34 +0100

    add-pckg-crypt

--- app/Http/Controllers/API/shopinerController.php
+++ app/Http/Controllers/API/shopinerController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\{User, posts, pings};
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Crypt;
 
 class shopinerController extends Controller
 {
