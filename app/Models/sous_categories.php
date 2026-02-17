5d84c703a28be2129336957dc3867e48be58c5c4
hazarnenni <hazarne14@gmail.com>
2026-02-17 13:32:29 +0100

    fix-getpost

--- app/Models/sous_categories.php
+++ app/Models/sous_categories.php
@@ -22,12 +22,16 @@ class sous_categories extends Model
 
 
 
-    public function getPost() {
-        return $this->hasMany(posts::class, 'id_sous_categorie','id');
+    public function getPost()
+    {
+        return $this->hasMany(posts::class, 'id_sous_categorie', 'id')
+            ->whereHas('user_info', function ($query) {
+                $query->where('voyage_mode', 0);
+            });
     }
 
     public function categorie()
     {
-        return $this->belongsTo(categories::class, 'id_categorie')->orderBy("order","asc");
+        return $this->belongsTo(categories::class, 'id_categorie')->orderBy("order", "asc");
     }
 }
