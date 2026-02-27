16eb04ed8932bcbc58acf8a49d31a8ae1703f8dc
hazarnenni <hazarne14@gmail.com>
2026-02-27 12:18:06 +0100

    fix-condition-politiq-conf

--- resources/views/User/composants/text-conditions.blade.php
+++ resources/views/User/composants/text-conditions.blade.php
@@ -1,4 +1,4 @@
-
+{{--
 <div style="{{ app()->getLocale() == 'ar' ? 'text-align: right; direction: rtl;' : 'text-align: left; direction: ltr;' }}">
     <h2>{{ __('general_conditions_title') }}</h2>
 
@@ -87,5 +87,270 @@
 
         {!! __('luxury_authentication_policy') !!}
     </p>
+
+</div> --}}
+<div style="{{ app()->getLocale() == 'ar' ? 'text-align: right; direction: rtl;' : 'text-align: left; direction: ltr;' }}">
+
+    <h2>{!! __('general_conditions_title1') !!}</h2>
+
+    {{-- PRÉAMBULE --}}
+    <h4>{!! __('preamble_title') !!}</h4>
+    <p>{!! __('preamble_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 1 --}}
+    <h4>1. {!! __('article1_title') !!}</h4>
+    <p>{!! __('article1_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 2 --}}
+    <h4>2. {!! __('article2_title') !!}</h4>
+    <p>{!! __('article2_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 3 --}}
+    <h4>3. {!! __('article3_title') !!}</h4>
+
+    <h5>{!! __('article3_role_subtitle') !!}</h5>
+    <p>{!! __('article3_role_content') !!}</p>
+
+    <h5>{!! __('article3_responsibility_subtitle') !!}</h5>
+    <p>{!! __('article3_responsibility_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 4 --}}
+    <h4>4. {!! __('article4_title') !!}</h4>
+
+    <h5>{!! __('article4_conditions_subtitle') !!}</h5>
+    <p>{!! __('article4_conditions_content') !!}</p>
+
+    <h5>{!! __('article4_process_subtitle') !!}</h5>
+    <p>{!! __('article4_process_content') !!}</p>
+
+    <h5>{!! __('article4_security_subtitle') !!}</h5>
+    <p>{!! __('article4_security_content') !!}</p>
+
+    <h5>{!! __('article4_suspension_subtitle') !!}</h5>
+    <p>{!! __('article4_suspension_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 5 --}}
+    <h4>5. {!! __('article5_title') !!}</h4>
+
+    <h5>{!! __('article5_publication_subtitle') !!}</h5>
+    <p>{!! __('article5_publication_content') !!}</p>
+
+    <h5>{!! __('article5_obligations_subtitle') !!}</h5>
+    <p>{!! __('article5_obligations_content') !!}</p>
+
+    <h5>{!! __('article5_forbidden_subtitle') !!}</h5>
+    <p>{!! __('article5_forbidden_content') !!}</p>
+
+    <h5>{!! __('article5_price_subtitle') !!}</h5>
+    <p>{!! __('article5_price_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 6 --}}
+    <h4>6. {!! __('article6_title') !!}</h4>
+
+    <h5>{!! __('article6_order_subtitle') !!}</h5>
+    <p>{!! __('article6_order_content') !!}</p>
+
+    <h5>{!! __('article6_confirmation_subtitle') !!}</h5>
+    <p>{!! __('article6_confirmation_content') !!}</p>
+
+    <h5>{!! __('article6_availability_subtitle') !!}</h5>
+    <p>{!! __('article6_availability_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 7 --}}
+    <h4>7. {!! __('article7_title') !!}</h4>
+
+    <h5>{!! __('article7_price_subtitle') !!}</h5>
+    <p>{!! __('article7_price_content') !!}</p>
+
+    <h5>{!! __('article7_payment_subtitle') !!}</h5>
+    <p>{!! __('article7_payment_content') !!}</p>
+
+    <h5>{!! __('article7_distribution_subtitle') !!}</h5>
+    <p>
+        <i>{!! __('seller_commission_intro1') !!}</i><br><br>
+        <b>
+            {!! __('luxury_brand_commission1') !!}<br>
+            {!! __('other_category_commission1') !!}<br>
+        </b>
+        <br>
+        {!! __('delivery_fees_not_included1') !!}
+    </p>
+
+    <h5>{!! __('article7_delivery_fees_subtitle') !!}</h5>
+    <p>{!! __('article7_delivery_fees_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 8 --}}
+    <h4>8. {!! __('article8_title') !!}</h4>
+
+    <h5>{!! __('article8_modalities_subtitle') !!}</h5>
+    <p>{!! __('article8_modalities_content') !!}</p>
+
+    <h5>{!! __('article8_tracking_subtitle') !!}</h5>
+    <p>{!! __('article8_tracking_content') !!}</p>
+
+    <h5>{!! __('article8_reception_subtitle') !!}</h5>
+    <p>{!! __('article8_reception_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 9 --}}
+    <h4>9. {!! __('article9_title') !!}</h4>
+
+    <h5>{!! __('article9_withdrawal_subtitle') !!}</h5>
+    <p>{!! __('article9_withdrawal_content') !!}</p>
+
+    <h5>{!! __('article9_excluded_subtitle') !!}</h5>
+    <p>{!! __('article9_excluded_content') !!}</p>
+
+    <h5>{!! __('article9_return_fees_subtitle') !!}</h5>
+    <p>{!! __('article9_return_fees_content') !!}</p>
+
+    <h5>{!! __('article9_refund_subtitle') !!}</h5>
+    <p>{!! __('article9_refund_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 10 --}}
+    <h4>10. {!! __('article10_title') !!}</h4>
+
+    <h5>{!! __('article10_conformity_subtitle') !!}</h5>
+    <p>{!! __('article10_conformity_content') !!}</p>
+
+    <h5>{!! __('article10_hidden_defects_subtitle') !!}</h5>
+    <p>{!! __('article10_hidden_defects_content') !!}</p>
+
+    <h5>{!! __('article10_limitation_subtitle') !!}</h5>
+    <p>{!! __('article10_limitation_content') !!}</p>
+
+    <h5>{!! __('article10_platform_subtitle') !!}</h5>
+    <p>{!! __('article10_platform_content') !!}</p>
+
+    <h5>{!! __('article10_force_majeure_subtitle') !!}</h5>
+    <p>{!! __('article10_force_majeure_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 11 --}}
+    <h4>11. {!! __('article11_title') !!}</h4>
+
+    <h5>{!! __('article11_general_subtitle') !!}</h5>
+    <p>{!! __('article11_general_content') !!}</p>
+
+    <h5>{!! __('article11_forbidden_subtitle') !!}</h5>
+    <p>{!! __('article11_forbidden_content') !!}</p>
+
+    <h5>{!! __('article11_sanctions_subtitle') !!}</h5>
+    <p>{!! __('article11_sanctions_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 12 --}}
+    <h4>12. {!! __('article12_title') !!}</h4>
+
+    <h5>{!! __('article12_platform_subtitle') !!}</h5>
+    <p>{!! __('article12_platform_content') !!}</p>
+
+    <h5>{!! __('article12_license_subtitle') !!}</h5>
+    <p>{!! __('article12_license_content') !!}</p>
+
+    <h5>{!! __('article12_ugc_subtitle') !!}</h5>
+    <p>{!! __('article12_ugc_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 13 --}}
+    <h4>13. {!! __('article13_title') !!}</h4>
+    <p>{!! __('article13_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 14 --}}
+    <h4>14. {!! __('article14_title') !!}</h4>
+    <p>{!! __('article14_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 15 --}}
+    <h4>15. {!! __('article15_title') !!}</h4>
+
+    <h5>{!! __('article15_contact_subtitle') !!}</h5>
+    <p>{!! __('article15_contact_content') !!}</p>
+
+    <h5>{!! __('article15_law_subtitle') !!}</h5>
+    <p>{!! __('article15_law_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 16 --}}
+    <h4>16. {!! __('article16_title') !!}</h4>
+
+    <h5>{!! __('article16_partial_subtitle') !!}</h5>
+    <p>{!! __('article16_partial_content') !!}</p>
+
+    <h5>{!! __('article16_entirety_subtitle') !!}</h5>
+    <p>{!! __('article16_entirety_content') !!}</p>
+
+    <h5>{!! __('article16_waiver_subtitle') !!}</h5>
+    <p>{!! __('article16_waiver_content') !!}</p>
+
+    <h5>{!! __('article16_language_subtitle') !!}</h5>
+    <p>{!! __('article16_language_content') !!}</p>
+
+    <br>
+
+    {{-- ARTICLE 17 --}}
+    <h4>17. {!! __('article17_title') !!}</h4>
+    <p>{!! __('article17_content') !!}</p>
+
+    <br>
+
+    {{-- SECTION LUXE SHOPIN --}}
+    <h4>{!! __('luxury_title1') !!}</h4>
+    <p>
+        <i>{!! __('luxury_description1') !!}</i><br><br>
+
+        {!! __('luxury_price_condition1') !!} {!! __('currency1') !!}<br><br>
+
+        {!! __('luxury_authentication_condition1') !!}<br><br>
+
+        <i>{!! __('luxury_category_condition1') !!}</i><br>
+
+        <b>
+            {!! __('luxury_symbol_authentication1') !!}
+            <i class="bi bi-gem" style="color: #008080; font-weight: bold;"></i>
+        </b><br><br>
+
+        <i>{!! __('luxury_sale_steps1') !!}</i><br>
+
+        &mdash; {!! __('luxury_publish_item1') !!}<br>
+        &mdash; {!! __('luxury_purchase_confirmation1') !!}<br>
+        &mdash; {!! __('luxury_item_authentication1') !!}<br>
+        &mdash; {!! __('luxury_item_delivery1') !!}<br>
+        &mdash; {!! __('luxury_payment_confirmation1') !!}<br><br>
+
+        {!! __('luxury_return_policy1') !!}<br><br>
+
+        {!! __('luxury_authentication_policy1') !!}
+    </p>
+
+    <br>
+
+    <p><small>{!! __('last_update') !!}</small></p>
 
 </div>
--- resources/views/User/politique-confidentialite.blade.php
+++ resources/views/User/politique-confidentialite.blade.php
@@ -3,5 +3,133 @@
 @section('content')
 @section('body')
 
+<div class="container pt-5 pb-5" dir="{{ in_array(App::getLocale(), ['ar', 'fa']) ? 'rtl' : 'ltr' }}">
+    <div style="{{ app()->getLocale() == 'ar' ? 'text-align: right; direction: rtl;' : 'text-align: left; direction: ltr;' }}">
+
+        <h2>{!! __('privacy_title') !!}</h2>
+
+        {{-- PRÉAMBULE --}}
+        <h4>{!! __('privacy_preamble_title') !!}</h4>
+        <p>{!! __('privacy_preamble_content') !!}</p>
+
+        <br>
+
+        {{-- ARTICLE 1 - DÉFINITIONS --}}
+        <h4>1. {!! __('privacy_article1_title') !!}</h4>
+        <p>{!! __('privacy_article1_content') !!}</p>
+
+        <br>
+
+        {{-- ARTICLE 2 - DONNÉES COLLECTÉES --}}
+        <h4>2. {!! __('privacy_article2_title') !!}</h4>
+
+        <h5>{!! __('privacy_article2_direct_subtitle') !!}</h5>
+        <p>
+            {!! __('privacy_article2_direct_all_users') !!}
+            <br><br>
+            {!! __('privacy_article2_direct_sellers') !!}
+            <br><br>
+            {!! __('privacy_article2_direct_buyers') !!}
+        </p>
+
+        <h5>{!! __('privacy_article2_auto_subtitle') !!}</h5>
+        <p>{!! __('privacy_article2_auto_content') !!}</p>
+
+        <br>
+
+        {{-- ARTICLE 3 - FINALITÉS --}}
+        <h4>3. {!! __('privacy_article3_title') !!}</h4>
+
+        <h5>{!! __('privacy_article3_main_subtitle') !!}</h5>
+        <p>{!! __('privacy_article3_main_content') !!}</p>
+
+        <h5>{!! __('privacy_article3_secondary_subtitle') !!}</h5>
+        <p>{!! __('privacy_article3_secondary_content') !!}</p>
+
+        <br>
+
+        {{-- ARTICLE 4 - DESTINATAIRES --}}
+        <h4>4. {!! __('privacy_article4_title') !!}</h4>
+
+        <h5>{!! __('privacy_article4_internal_subtitle') !!}</h5>
+        <p>{!! __('privacy_article4_internal_content') !!}</p>
+
+        <h5>{!! __('privacy_article4_partners_subtitle') !!}</h5>
+        <p>{!! __('privacy_article4_partners_content') !!}</p>
+
+        <h5>{!! __('privacy_article4_no_sale_subtitle') !!}</h5>
+        <p>{!! __('privacy_article4_no_sale_content') !!}</p>
+
+        <br>
+
+        {{-- ARTICLE 5 - SÉCURITÉ --}}
+        <h4>5. {!! __('privacy_article5_title') !!}</h4>
+
+        <h5>{!! __('privacy_article5_measures_subtitle') !!}</h5>
+        <p>{!! __('privacy_article5_measures_content') !!}</p>
+
+        <h5>{!! __('privacy_article5_encryption_subtitle') !!}</h5>
+        <p>{!! __('privacy_article5_encryption_content') !!}</p>
+
+        <h5>{!! __('privacy_article5_user_subtitle') !!}</h5>
+        <p>{!! __('privacy_article5_user_content') !!}</p>
+
+        <br>
+
+        {{-- ARTICLE 6 - COOKIES --}}
+        <h4>6. {!! __('privacy_article6_title') !!}</h4>
+
+        <h5>{!! __('privacy_article6_what_subtitle') !!}</h5>
+        <p>{!! __('privacy_article6_what_content') !!}</p>
+
+        <h5>{!! __('privacy_article6_types_subtitle') !!}</h5>
+        <p>{!! __('privacy_article6_types_content') !!}</p>
+
+        <h5>{!! __('privacy_article6_manage_subtitle') !!}</h5>
+        <p>{!! __('privacy_article6_manage_content') !!}</p>
+
+        <br>
+
+        {{-- ARTICLE 7 - VOS DROITS --}}
+        <h4>7. {!! __('privacy_article7_title') !!}</h4>
+
+        <p><i>{!! __('privacy_article7_intro') !!}</i></p>
+
+        <h5>{!! __('privacy_article7_access_subtitle') !!}</h5>
+        <p>{!! __('privacy_article7_access_content') !!}</p>
+
+        <h5>{!! __('privacy_article7_rectification_subtitle') !!}</h5>
+        <p>{!! __('privacy_article7_rectification_content') !!}</p>
+
+        <h5>{!! __('privacy_article7_erasure_subtitle') !!}</h5>
+        <p>{!! __('privacy_article7_erasure_content') !!}</p>
+
+        <h5>{!! __('privacy_article7_opposition_subtitle') !!}</h5>
+        <p>{!! __('privacy_article7_opposition_content') !!}</p>
+
+        <h5>{!! __('privacy_article7_limitation_subtitle') !!}</h5>
+        <p>{!! __('privacy_article7_limitation_content') !!}</p>
+
+        <h5>{!! __('privacy_article7_exercise_subtitle') !!}</h5>
+        <p>{!! __('privacy_article7_exercise_content') !!}</p>
+
+        <br>
+
+        {{-- ARTICLE 8 - MODIFICATIONS --}}
+        <h4>8. {!! __('privacy_article8_title') !!}</h4>
+        <p>{!! __('privacy_article8_content') !!}</p>
+
+        <br>
+
+        {{-- ARTICLE 9 - DÉCLARATION CNDP --}}
+        <h4>9. {!! __('privacy_article9_title') !!}</h4>
+        <p>{!! __('privacy_article9_content') !!}</p>
+
+        <br>
+
+        <p><small>{!! __('privacy_last_update') !!}</small></p>
+
+    </div>
+</div>
 
 @endsection
