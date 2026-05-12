ef36c58385183fb8a0fe79175d85cc892f4971b0
hazarnenni <hazarne14@gmail.com>
2026-05-12 12:31:08 +0100

    fix-notif

--- app/Livewire/User/UpdateInformations.php
+++ app/Livewire/User/UpdateInformations.php
@@ -146,7 +146,7 @@ class UpdateInformations extends Component
         $user->num_appartement = $this->num_appartement;
         $user->save();
 
-        $this->dispatch('alert', ['message' => __('info_updated') . ($photoValidationMessage ?? ''), 'type' => 'info']);
+        $this->dispatch('alert', ['message' => __('info_updated'), 'type' => 'info']);
 
         $this->dispatch('refreshAlluser-information');
         if (session()->has('redirect_after_profile')) {
