acc6a90fc85eca3dc4e817d4e40258c4086ab4c7
hazarnenni <hazarne14@gmail.com>
2026-05-29 17:53:48 +0100

    fix-design-search

--- resources/views/livewire/user/shopinners.blade.php
+++ resources/views/livewire/user/shopinners.blade.php
@@ -1,24 +1,38 @@
 <div>
      <div class="row" dir="{{ in_array(App::getLocale(), ['ar', 'fa']) ? 'rtl' : 'ltr' }}">
-         <div class="col-md-12">
-             <div class="d-flex justify-content-end align-items-center gap-2">
-                 <div class="me-2">
-                     <input type="text" wire:model.live="key" class="form-control"
-                         placeholder="{!! \App\Traits\TranslateTrait::TranslateText('Trouvez votre shopiner idéal') !!}">
-                 </div>
-                 <div>
-                     <select wire:model.live="rating" class="form-control custom-select">
-                         <option value="">{!! \App\Traits\TranslateTrait::TranslateText('Filtrer par avis') !!}</option>
-                         <option value="1">★ 1 {!! \App\Traits\TranslateTrait::TranslateText('Étoile et plus') !!}</option>
-                         <option value="2">★★ 2 {!! \App\Traits\TranslateTrait::TranslateText('Étoiles et plus') !!}</option>
-                         <option value="3">★★★ 3 {!! \App\Traits\TranslateTrait::TranslateText('Étoiles et plus') !!}</option>
-                         <option value="4">★★★★ 4 {!! \App\Traits\TranslateTrait::TranslateText('Étoiles et plus') !!}</option>
-                         <option value="5">★★★★★ 5 {!! \App\Traits\TranslateTrait::TranslateText('Étoiles') !!}</option>
-                     </select>
-                 </div>
-             </div>
-         </div>
-     </div>
+        <div class="col-md-12">
+            <div class="filter-card">
+
+                <!-- Search -->
+                <div class="filter-item search-box">
+                    <i class="fa fa-search"></i>
+
+                    <input type="text"
+                        wire:model.live="key"
+                        class="filter-input"
+                        placeholder="{!! \App\Traits\TranslateTrait::TranslateText('Trouvez votre shopiner idéal') !!}">
+                </div>
+
+                <!-- Rating -->
+                <div class="filter-item select-box">
+                    <i class="fa fa-star"></i>
+
+                    <select wire:model.live="rating" class="filter-select">
+                        <option value="">
+                            {!! \App\Traits\TranslateTrait::TranslateText('Filtrer par avis') !!}
+                        </option>
+
+                        <option value="1">★ 1 {!! \App\Traits\TranslateTrait::TranslateText('Étoile et plus') !!}</option>
+                        <option value="2">★★ 2 {!! \App\Traits\TranslateTrait::TranslateText('Étoiles et plus') !!}</option>
+                        <option value="3">★★★ 3 {!! \App\Traits\TranslateTrait::TranslateText('Étoiles et plus') !!}</option>
+                        <option value="4">★★★★ 4 {!! \App\Traits\TranslateTrait::TranslateText('Étoiles et plus') !!}</option>
+                        <option value="5">★★★★★ 5 {!! \App\Traits\TranslateTrait::TranslateText('Étoiles') !!}</option>
+                    </select>
+                </div>
+
+            </div>
+        </div>
+    </div>
      <br>
 
      <div class="row" dir="{{ in_array(App::getLocale(), ['ar', 'fa']) ? 'rtl' : 'ltr' }}">
