5f1e4e1ac575d159c5ce263fc6a18b8273527dc3
hazarnenni <hazarne14@gmail.com>
2026-07-25 23:07:17 +0100

    fix-prix

--- resources/views/components/Liste-mes-posts.blade.php
+++ resources/views/components/Liste-mes-posts.blade.php
@@ -497,13 +497,13 @@
                         <td>
                             {{ $item->old_prix ?? $item->prix }} <sup>{{ __('currency') }}</sup>
                         </td>
-                        <td>
+                        {{-- <td>
                             @php
                                 $basePx = ($item->getOldPrix() && $item->getOldPrix() > $item->getPrix()) ? $item->getOldPrix() : $item->getPrix();
                             @endphp
                             <span class="price-new">{{ $basePx }} <sup>{{ __('currency') }}</sup></span>
-                        </td>
-                        {{-- <td>
+                        </td> --}}
+                        <td>
                             @if ($item->getOldPrix() && $item->getOldPrix() > $item->getPrix())
                                 <span class="price-new">{{ $item->getPrix() }} <sup>{{ __('currency') }}</sup></span>
                                 <br>
@@ -513,7 +513,7 @@
                             @else
                                 <span class="price-new">{{ $item->getPrix() }} <sup>{{ __('currency') }}</sup></span>
                             @endif
-                        </td> --}}
+                        </td>
 
                         <td>
                             <div class="date-cell">
